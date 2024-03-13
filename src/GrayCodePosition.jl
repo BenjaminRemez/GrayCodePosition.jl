1d36855e7c8bdba42db66282138eaf17dd39f018
Benjamin Remez <bremez@gmail.com>
2024-03-13 11:36:06 -0400

    Initial commit: complete functionality

--- src/GrayCodePosition.jl
+++ src/GrayCodePosition.jl
@@ -1,5 +1,40 @@
 module GrayCodePosition
 
-# Write your package code here.
+import Base: eltype, length, diff, iterate
+
+export GrayCode
+
+struct GrayCode
+    nbits::Int64
+    function GrayCode(n::Integer)
+        n <= 0 && Base.throw(ArgumentError("Code must have positive length!")) 
+        new(n)
+    end
+end
+eltype(::GrayCode) = Int64
+diff(code::GrayCode) = GrayCodePositions(code)
+length(code::GrayCode) = (1 << code.nbits)
+iterate(code::GrayCode) = (0,1)
+function iterate(code::GrayCode, state)  
+ state == length(code) && return nothing
+ (state ⊻ (state >> 1),state + 1)
+end
+
+struct GrayCodePositions
+    code::GrayCode
+end
+const GCP = GrayCodePositions;
+
+
+eltype(::GCP) = Int64; 
+# Note the varying positions are one less than the total number of codes
+length(gcp::GCP) = length(gcp.code) - 1
+iterate(gcp::GCP) =  (one(eltype(gcp)), 1)
+
+function Base.iterate(gcp::GCP, state) 
+    state == length(gcp) && return nothing
+    n = state + 1;
+    (trailing_zeros(n) + 1, n)
+end 
 
 end
